4165ad422dc73edddf9bfbb07bcbdf8c8c9b30c7
Y.Abouqora <yabouqora@gmail.com>
2024-12-23 10:54:27 +0100

    first commit

---
+++ src/main/java/com/example/kafkajavaexample/KafkaJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkajavaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJavaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaJavaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkajavaexample/KafkaJavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkajavaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJavaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
